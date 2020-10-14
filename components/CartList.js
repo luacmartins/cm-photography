bedbf078b9a14419c4e41d104b9c43e5287d4cbf
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2020-10-14 10:57:55 -0600

    fix cartdropdown on load layout

--- components/CartList.js
+++ components/CartList.js
@@ -14,13 +14,13 @@ const CartList = ({ removeButton, bg, disabled }) => {
    return (
       <div className="sm:px-0 sm:flex sm:flex-1 sm:overflow-y-hidden">
          <div className="flex flex-col flex-1 pb-4 mb-24 sm:mb-0 sm:max-h-96 sm:overflow-scroll sm:px-2">
+            {cartItems.length > 0 && cartItems.map((product, index) => (
+               <CartCard key={index} id={index} product={product} removeButton={removeButton} bg={bg} disabled={disabled} />
+            ))}
             {cartItems.length < 1 && <div className="flex flex-col">
                <div className="mt-8 text-center">Your cart is empty!</div>
                <button onClick={handleClick} className="mt-12 underline cursor-pointer">Track Orders</button>
             </div>}
-            {cartItems.length >= 1 && cartItems.map((product, index) => (
-               <CartCard key={index} id={index} product={product} removeButton={removeButton} bg={bg} disabled={disabled} />
-            ))}
          </div>
       </div>
    );
