933d8f55f5226d1189ddc66ea0010ce22d1ddbaf
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2020-10-14 15:25:25 -0600

    fix mobile scroll

--- components/Main.js
+++ components/Main.js
@@ -1,9 +1,33 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import NavBar from './NavBar'
 
 export default function Main({ children, title, bg = true }) {
    const [noScroll, setNoScroll] = useState(false)
+
+   useEffect(() => {
+      const getViewportHeight = () => {
+         const customViewportCorrectionVariable = 'vh';
+
+         function setViewportProperty(doc) {
+            let prevClientHeight;
+            let customVar = '--' + (customViewportCorrectionVariable || 'vh');
+            function handleResize() {
+               let clientHeight = doc.clientHeight;
+               if (clientHeight === prevClientHeight) return;
+               requestAnimationFrame(function updateViewportHeight() {
+                  doc.style.setProperty(customVar, (clientHeight * 0.01) + 'px');
+                  prevClientHeight = clientHeight;
+               });
+            }
+            handleResize();
+            return handleResize;
+         }
+         window.addEventListener('resize', setViewportProperty(document.documentElement));
+      }
+      getViewportHeight()
+   })
+
 
    return (
       <>
